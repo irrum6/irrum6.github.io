34250359674bca6fa63e67f2a8937d08fb2ed4f6
Irakli Mindorashvili <imindorashvili@silknet.com>
2018-03-01 17:27:35 +0400

    dft2

--- screencalc/index1.js
+++ screencalc/index1.js
@@ -12,7 +12,7 @@ ScreenCalc.EnableResolutionInput = function (reverse) {
     }
 };
 
-ScreenCalc.WatchVal = function () {
+ScreenCalc.WatchVal = function (event) {
     let val = parseFloat(event.target.value);
     let max = parseFloat(event.target.max);
     let min = parseFloat(event.target.min);
@@ -37,4 +37,99 @@ ScreenCalc.ShowMessage = function (message, reverse) {
         el.parentElement.style.visibility = "visible";
     }
 
+};
+
+ScreenCalc.CalcRatio = function (height, width, normalize) {
+    if (Lib.isPositiveInteger(height, width)) {
+        let ratio = height / width;
+        let ratioPrecision = Lib.toPrecision(ratio, 2);
+        if (normalize) {
+            let wideAspect = Lib.toPrecision((ratio * 9), 1);
+            let normalizedRatio = (wideAspect + "/9").replace('.0', '');
+            return { success: true, ratio: ratio, correctedRatio: ratioPrecision, normalizedRatio: normalizedRatio };
+        }
+        return { success: true, ratio: ratio };
+    }
+    return { success: false };
+};
+
+ScreenCalc.Calc = function () {
+    let [ratio, correctedRatio, normalizedRatio] = [1, 1, 1];
+
+    let [diagSize, resLength, resWidth, providedRatio] = ['#diag', '#height', '#width', '#ratio'].map((elem) => {
+        return Lib.q(elem).value;
+    });
+
+    providedRatio = providedRatio.split('/');
+
+    if (diagSize.length === 0) {
+        Lib.q('#diagsize').classList.add('warning');
+        showMessage('Enter diagonal size');
+    } else {
+        let normalize = true;
+        resLength = parseInt(resLength, 10);
+        resWidth = parseInt(resWidth, 10);
+
+        let calcratio = ScreenCalc.CalcRatio(resLength, resWidth, true);
+
+        if (calcratio.success) {
+            ratio = calcratio.ratio;
+            correctedRatio = calcratio.correctedRatio;
+            normalizedRatio = calcratio.normalizedRatio;
+        }
+
+        let diagSizemm = diagSize * 25.4;
+        let lengthmm = 1;
+        let widthmm = 1;
+        let screenArea = 1;
+
+        //compare provided and actual ratios
+        if ((providedRatio !== ratio) && ratio > 1) {
+            //use calculated ratio
+            widthmm = Math.sqrt((diagSizemm * diagSizemm) / (1 + (ratio * ratio)));
+            lengthmm = widthmm * ratio;
+        } else {
+            //use provided ratio
+            let ratio1 = providedRatio[1];
+            let ratio2 = providedRatio[0];
+            widthmm = Math.sqrt((diagSizemm * diagSizemm) / ((ratio1 * ratio1) + (ratio2 * ratio2))) * ratio1;
+            lengthmm = (widthmm / ratio1) * ratio2;
+        }
+        screenArea = widthmm * lengthmm;
+
+        let updateObj = {
+            diag: diagSize,
+            diagmm: diagSizemm,
+            wid: widthmm,
+            len: lengthmm,
+            ratio: providedRatio.join('/'),
+            cratio: correctedRatio,
+            ncratio: normalizedRatio,
+            area: screenArea
+        };
+
+        ScreenCalc.Update(updateObj);
+    }
+};
+
+ScreenCalc.Update = function (updateObj) {
+    Lib.q('#diag-par').textContent = 'Diagonal Size : $i inches ($m mm)'
+        .replace("$i", updateObj.diag).replace("$m", updateObj.diagmm);
+    Lib.q('#inp-ratio-par').textContent = 'Inputed Aspect Ratio : $ratio'
+        .replace("$ratio", updateObj.ratio);
+    Lib.q('#calc-ratio-par').textContent = 'Calculated Aspect Ratio : $cratio'
+        .replace("$cratio", updateObj.cratio);
+    Lib.q('#calc-ratio-norm-par').textContent = 'Calculated Aspect Ratio (Normalized to 16/9 format) : $ncratio'
+        .replace("$ncratio", updateObj.ncratio);
+    Lib.q('#calc-width-par').textContent = 'Calculated Width:$m mm ($i inches)'
+        .replace("$m", Lib.toPrecision(updateObj.wid, 1))
+        .replace("$i", Lib.toPrecision(updateObj.wid / 25.4, 1));
+    Lib.q('#calc-height-par').textContent = 'Calculated Height:$m mm ($i inches)'
+        .replace("$m", Lib.toPrecision(updateObj.len, 1))
+        .replace("$i", Lib.toPrecision(updateObj.len / 25.4, 1));
+    Lib.q('#calc-area-par').innerHTML = 'Calculated Area: $m mm<sup>2</sup> $cm cm<sup>2</sup> ($i inch<sup>2</sup>)'
+        .replace("$m", Lib.toPrecision(updateObj.area, 1))
+        .replace("$cm", Lib.toPrecision(updateObj.area / 100, 1))
+        .replace("$i", Lib.toPrecision((updateObj.len / 25.4) / 25.4, 1));
+    Lib.q('#result').style.visibility = "visible";
 };
