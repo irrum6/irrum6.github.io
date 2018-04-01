81deddf7b2dd662e6edfc5c68f29814bead407ca
puree92 <puree92@github.com>
2018-04-02 00:27:54 +0400

    automatic switches

---
+++ screencalc/new/ttoggle.js
@@ -0,0 +1,55 @@
+let switchers = document.body.querySelectorAll('.span-switcher');
+let len = switchers.length;
+for (let i = 0; i < len; i++) {
+    switchers[i].addEventListener('click', (event) => {
+        let target = event.target.parentElement;
+
+        if (target.hasAttribute('data-app-switch-disabled') && target.getAttribute('data-app-switch-disabled') === '1') {
+            //just do nothing
+        } else {
+            //if not disabled 
+            let id = target.getAttribute('id');
+            let contrids = target.getAttribute('data-app-switch-contra').split(',');
+            if (id !== null) {
+                Lib.q(('#$id'.replace('$id', id))).dispatchEvent(new CustomEvent('toggle', { detail: 'toggle' }));
+            }
+            for (let i = 0, len = contrids.length; i < len; i++) {
+                if (contrids[i] !== null && contrids[i] !== '#') {
+                    Lib.q(('#$cid'.replace('$cid', contrids[i]))).dispatchEvent(new CustomEvent('toggle', { detail: 'toggle' }));
+                }
+            }
+        }
+
+    });
+    switchers[i].addEventListener('toggle', (event) => {
+        let target = event.target;
+        //current status /computing /using input
+        let switchon = target.getAttribute('data-app-switch-value');
+        let isComputing = (switchon === '1') ? true : false;
+        let linked = target.getAttribute('data-app-linked-inputs').split(',');
+        if (isComputing) {
+            //switch to using input
+            //switch the flag
+            target.setAttribute('data-app-switch-value', '0');
+            //enable inputs
+            for (let i = 0, len = linked.length; i < len; i++) {
+                let selector = '[data-app-reference="#ref"]'.replace("#ref", linked[i]);
+                Lib.q(selector).disabled = false;
+            }
+            //visual flag change
+            target.children[0].style.display = "none";
+            target.children[1].style.display = "flex";
+        } else {
+            //switch to use computed value
+            target.setAttribute('data-app-switch-value', '1');
+            //disable inputs
+            for (let i = 0, len = linked.length; i < len; i++) {
+                let selector = '[data-app-reference="#ref"]'.replace("#ref", linked[i]);
+                Lib.q(selector).disabled = true;
+            }
+            //visual flag change
+            target.children[0].style.display = "flex";
+            target.children[1].style.display = "none";
+        }
+    });
+}
