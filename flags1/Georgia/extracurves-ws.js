ba24c846d5cf8a14e4e856a94d9591dd19e2a8fa
puree92 <puree92@github.com>
2018-05-10 19:26:16 +0400

    draft, top-left cross finished

---
+++ flags1/Georgia/extracurves-ws.js
@@ -0,0 +1,23 @@
+/*
+topleft cross
+*/
+//top edge
+window.setTimeout(function () {
+    hlineCurverExta('TopLeftCrossTopEdgeLeft');
+    hlineCurverExta('TopLeftCrossTopEdgeRight');
+}, m * 18);
+//left edge
+window.setTimeout(function () {
+    vlineCurverExta('TopLeftCrossLeftEdgeTop');
+    vlineCurverExta('TopLeftCrossLeftEdgeBottom');
+}, m * 18);
+//right edge
+window.setTimeout(function () {
+    vlineCurverExta('TopLeftCrossRightEdgeTop');
+    vlineCurverExta('TopLeftCrossRightEdgeBottom');
+}, m * 18);
+//bottom edge
+window.setTimeout(function () {
+    hlineCurverExta('TopLeftCrossBottomEdgeLeft');
+    hlineCurverExta('TopLeftCrossBottomEdgeRight');
+}, m * 18);
--- flags1/Georgia/script.js
+++ flags1/Georgia/script.js
@@ -11,38 +11,39 @@ window.setTimeout(function () {
         cssClassList: ['red', 'line']
     }));
 }, 3000);
-
+//dial m for multiplier
+let m = 2000;
 window.setTimeout(function () {
     hline('topLeftHorizontal');
-}, 6000);
+}, m * 2);
 
 window.setTimeout(function () {
     vline('topLeftVertical');
-}, 9000);
+}, m * 3);
 
 window.setTimeout(function () {
     hline('topRightHorizontal');
-}, 12000);
+}, m * 4);
 
 window.setTimeout(function () {
     vline('topRightVertical');
-}, 15000);
+}, m * 5);
 
 window.setTimeout(function () {
     hline('bottomLeftHorizontal');
-}, 18000);
+}, m * 6);
 
 window.setTimeout(function () {
     vline('bottomLeftVertical');
-}, 21000);
+}, m * 7);
 
 window.setTimeout(function () {
     hline('bottomRightHorizontal');
-}, 24000);
+}, m * 8);
 
 window.setTimeout(function () {
     vline('bottompRightVertical');
-}, 27000);
+}, m * 9);
 
 /*curving top left cross*/
 
@@ -50,23 +51,23 @@ window.setTimeout(function () {
 window.setTimeout(function () {
     vlineCurver('topLeftVerticalTopLeftArc');
     vlineCurver('topLeftVerticalBottomLeftArc');
-}, 30000);
+}, m * 10);
 
 window.setTimeout(function () {
     vlineCurver('topLeftVerticalTopRightArc');
     vlineCurver('topLeftVerticalBottomRightArc');
-}, 33000);
+}, m * 10);
 
 //horizontal
 window.setTimeout(function () {
     hlineCurver('topLeftHorizontalTopLeftArc');
     hlineCurver('topLeftHorizontalTopRightArc');
-}, 36000);
+}, m * 11);
 
 window.setTimeout(function () {
     hlineCurver('topLeftHorizontalBottomLeftArc');
     hlineCurver('topLeftHorizontalBottomRightArc');
-}, 39000);
+}, m * 11);
 
 /*curving top right cross*/
 
@@ -74,23 +75,23 @@ window.setTimeout(function () {
 window.setTimeout(function () {
     vlineCurver('topRightVerticalTopLeftArc');
     vlineCurver('topRightVerticalBottomLeftArc');
-}, 42000);
+}, m * 12);
 
 window.setTimeout(function () {
     vlineCurver('topRightVerticalTopRightArc');
     vlineCurver('topRightVerticalBottomRightArc');
-}, 45000);
+}, m * 12);
 
 //horizontal
 window.setTimeout(function () {
     hlineCurver('topRightHorizontalTopLeftArc');
     hlineCurver('topRightHorizontalTopRightArc');
-}, 48000);
+}, m * 13);
 
 window.setTimeout(function () {
     hlineCurver('topRightHorizontalBottomLeftArc');
     hlineCurver('topRightHorizontalBottomRightArc');
-}, 51000);
+}, m * 13);
 
 
 /*curving bottom left cross*/
@@ -99,44 +100,43 @@ window.setTimeout(function () {
 window.setTimeout(function () {
     vlineCurver('bottomLeftVerticalTopLeftArc');
     vlineCurver('bottomLeftVerticalBottomLeftArc');
-}, 54000);
+}, m * 14);
 
 window.setTimeout(function () {
     vlineCurver('bottomLeftVerticalTopRightArc');
     vlineCurver('bottomLeftVerticalBottomRightArc');
-}, 57000);
+}, m * 14);
 
 window.setTimeout(function () {
     hlineCurver('bottomLeftHorizontalTopLeftArc');
     hlineCurver('bottomLeftHorizontalTopRightArc');
-}, 60000);
+}, m * 15);
 
 window.setTimeout(function () {
     hlineCurver('bottomLeftHorizontalBottomLeftArc');
     hlineCurver('bottomLeftHorizontalBottomRightArc');
-}, 63000);
+}, m * 15);
 
 //horizontal line
 window.setTimeout(function () {
     vlineCurver('bottomRightVerticalTopLeftArc');
     vlineCurver('bottomRightVerticalbottomRightArc');
-}, 66000);
+}, m * 16);
 
 window.setTimeout(function () {
     vlineCurver('bottomRightVerticalTopRightArc');
     vlineCurver('bottomRightVerticalBottomRightArc');
-}, 69000);
+}, m * 16);
 
 window.setTimeout(function () {
     hlineCurver('bottomRightHorizontalTopLeftArc');
     hlineCurver('bottomRightHorizontalTopRightArc');
-}, 72000);
+}, m * 17);
 
 window.setTimeout(function () {
     hlineCurver('bottomRightHorizontalbottomLeftArc');
     hlineCurver('bottomRightHorizontalBottomRightArc');
-}, 75000);
-
+}, m * 17);
 //functions
 function elem(options) {
     let element = document.createElement(options.tag);
@@ -176,5 +176,20 @@ function hlineCurver(id) {
     options.tag = 'div';
     options.id = id;
     options.cssClassList = ['white', 'line', 'hlineCurver'];
+    document.body.appendChild(elem(options));
+}
+
+function vlineCurverExta(id) {
+    let options = {};
+    options.tag = 'div';
+    options.id = id;
+    options.cssClassList = ['white', 'line', 'vlineCurverExtra'];
+    document.body.appendChild(elem(options));
+}
+function hlineCurverExta(id) {
+    let options = {};
+    options.tag = 'div';
+    options.id = id;
+    options.cssClassList = ['white', 'line', 'hlineCurverExtra'];
     document.body.appendChild(elem(options));
 }
