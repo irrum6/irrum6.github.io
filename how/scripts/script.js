06d4f694119fe45ef654f2e8e869263b3396f75f
irakli Mindorashvili <iraklim@pm.silknet.com>
2020-10-22 12:10:45 +0400

    rework slight

--- how/scripts/script.js
+++ how/scripts/script.js
@@ -1,223 +1,243 @@
-{
-    //query
-    let q = (selector) => document.body.querySelector(('' + selector));
-    //query all
-    let qa = (selector) => document.body.querySelectorAll(('' + selector));
+"use strict"
+//query
+let q = (s) => document.body.querySelector(('' + s));
+let qa = (s) => document.body.querySelectorAll(('' + s));
 
-    let buttons = qa("button");
+const isPositiveInteger = (num) => (typeof num === "number" && Number.isInteger(num) && num > -1);
+const strokefp = (fp, c) => fp.style.stroke = c;
 
-    /**
-     * @param {Number} num 
-     * @return {Boolean}
-     */
-    function isPositiveInteger(num) {
-        return (typeof num === "number" && Number.isInteger(num) && num > -1);
+class FigureHandler {
+    constructor() {
+        this.parts = q('svg').children;
     }
-
-    function getDomHelper(hangman) {
-        let helperInstance;
-        class helper {
-            constructor() {
-                this.how = hangman;
-            }
-            /**
-             * animate part of hangman figure
-             * @param {Number} index 
-             */
-            animateFigurePart(index) {
-                if (!isPositiveInteger(index)) return false;
-                q(`#part${index}`).style.stroke = "black";
-                return true;
-            }
-            /**
-             * @param {Number} index 
-             */
-            deAnimateFigurePart(index) {
-                if (!isPositiveInteger(index)) return false;
-                q(`#part${index}`).style.stroke = "transparent";
-                return true;
-            }
-            /**
-             * animate all figure parts
-             */
-            animateAllFigureParts() {
-                for (let i = 0; i < 9; i++) {
-                    this.animatePart(i);
-                }
-            }
-            deAnimateAllFigureParts() {
-                for (let i = 0; i < 9; i++) {
-                    this.deAnimateFigurePart(i);
-                }
-            }
-            /**
-             * setup event for keyboard (pc)
-             */
-            setupKeyboard() {
-                document.addEventListener('keydown', (e) => {
-                    for (let i = 0, len = buttons.length; i < len; i++) {
-                        if (buttons[i].innerHTML == e.key) {
-                            buttons[i].disabled = true;
-                        }
-                    }
-                    this.key(e.key)
-                });
-            }
-            /**
-             * since smartphones oncscreen keyboard takes much vertical space
-             * we use our virtual keyboard
-             */
-            SetupVirtualKeyboard() {
-                let len = buttons.length;
-                for (let i = 0; i < len; i++) {
-                    buttons[i].addEventListener('click', (e) => {
-                        e.target.disabled = true;
-                        let key = e.target.innerHTML;
-                        this.key(key);
-                    });
-                }
-            }
-            /**
-             * set the letter if on the dashboard
-             * @param {Number} index 
-             * @param {String} letter
-             * @return {Boolean}
-             */
-            SetLetter(index, letter) {
-                if (!isPositiveInteger(index) || (typeof letter !== "string" || letter.length > 1)) {
-                    return false;
-                }
-                q("#letter" + index).children[0].innerHTML = letter;
-                return true;
-            }
-            /**
-            * @param {Number} index 
-            */
-            SetKeyPassive(index) {
-                if (!isPositiveInteger(index)) return false;
-                buttons[index].disabled = true;
-                return true;
-            }
-            /**
-             * @param {Number} index 
-             */
-            SetKeyActive(index) {
-                if (!isPositiveInteger(index)) return false;
-                buttons[index].disabled = false;
-                return true;
-            }
-            /** 
-             * @param {Number} index 
-             */
-            SetInputOff(index) {
-                if (!isPositiveInteger(index)) return false;
-                q(`#letter${index} > line`).style.stroke = "transparent";
-                return true;
-            }
-            /**
-             * @param {Number} index 
-             */
-            SetInputOn(index) {
-                if (!isPositiveInteger(index)) return false;
-                q(`#letter${index} > line`).style.stroke = "black";
-                return true;
-            }
-            /**
-            * @param {char} key
-            */
-            key(key) {
-                console.log(key);
-                key = key.toLowerCase();
-                if (key == "space") key = " ";
-                console.log(this.how.letters);
-
-                if (this.how.letters.includes(key)) {
-                    console.log(11);
-                    for (let i = 0, len = this.how.letters.length; i < len; i++) {
-                        if (this.how.letters[i] === key) {
-                            this.SetLetter(i, key);
-                            this.how.letters[i] = null;
-                        }
-                    }
-                } else {
-                    this.animateFigurePart(this.how.count++);
-                    console.log(this.how.count);
-                }
-
-                let letters = this.how.letters.filter((elem, index) => {
-                    return elem !== null;
-                });
-                let len = letters.length;
-                if (len === 0) {
-                    let int = setTimeout(() => {
-                        this.how.loadLevel();
-                        clearTimeout(int)
-                    }, 2000);
-                }
-                if (this.how.count >= 8 && len > 0) {
-                    console.log(this.how.count);
-                    this.animateFigurePart(this.how.count);
-                    let int = setTimeout(() => {
-                        this.how.loadLevel();
-                        clearTimeout(int)
-                    }, 3000);
-                    for (let i = 0, leng = this.how.letters.length; i < leng; i++) {
-                        if (this.how.letters[i] !== null) {
-                            this.SetLetter(i + 1, this.how.letters[i]);
-                        }
-                    }
-                }
-            }
-        }
-        if (typeof helperInstance === 'undefined') {
-            helperInstance = new helper(hangman);
-        }
-        return helperInstance;
+    _get(index) {
+        return this.parts[index];
     }
-
-    let helper;
-
-    class HangMan {
-        constructor() {
-            this.count = 0;
-            this.letters = [];
-            this.scale = 1;
-        }
-        setup() {
-            helper = getDomHelper(this);
-            helper.setupKeyboard();
-            helper.SetupVirtualKeyboard();
-        }
-        loadLevel() {
-            this.count = 0;
-            let rand = Math.floor(Math.random() * words.length);
-            let word = words[rand];
-            q("#hint>span").innerHTML = word.type;
-
-            this.letters = word.name.split("");
-
-            let len = this.letters.length;
-
-            helper.deAnimateAllFigureParts();
-
-            for (let i = 0; i < 26; i++) {
-                //this clear input dashes
-                if (i < 12) {
-                    helper.SetInputOn(i);
-                    helper.SetLetter(i, "");
-                    if (i > len - 1) {
-                        helper.SetInputOff(i);
-                    }
-                }
-                //these clear keyboard
-                helper.SetKeyActive(i);
-            }
-            helper.SetLetter(0, this.letters[0]);
-
-            this.letters[0] = null;
+    animate(index) {
+        strokefp(this._get(index), "black",);
+    }
+    deAnimate(index) {
+        strokefp(this._get(index), "transparent",);
+    }
+    animateAll() {
+        for (const p of this.parts) {
+            strokefp(p, "black");
         }
     }
-    let hm = new HangMan();
-    hm.setup();
-    hm.loadLevel();
+    deAnimateAll() {
+        for (const p of this.parts) {
+            strokefp(p, "transparent");
+        }
+    }
 }
+
+class InputHandler {
+    constructor() {
+        this.inputs = qa("#input > span");
+        this.len = this.inputs.length;
+    }
+    show(i) {
+        this.inputs[i].textContent = "_";
+        this.inputs[i].style.display = "inline";
+    }
+    hide(i) {
+        this.inputs[i].style.display = "none";
+    }
+    setLetter(index, letter) {
+        letter = letter.toLowerCase();
+        if (!this.isValidChar(letter)) {
+            return false;
+        }
+        this.inputs[index].textContent = letter;
+    }
+    isValidChar(c) {
+        return /[a-z_]/.test(c);
+    }
+    clear() {
+        for (const inp of this.inputs) {
+            inp.textContent = "";
+        }
+    }
+    showOnlyXInputs(x) {
+        // debugger;
+        if (!isPositiveInteger(x)) {
+            return false;
+        }
+        for (let i = 0; i < this.len; i++) {
+            i > x ? this.hide(i) : this.show(i);
+        }
+    }
+}
+
+class VirtualKeyboard {
+    constructor() {
+        this.chars = "abcdefghijklmnopqrstuvwxyz".split("");
+        this.chars.push("space");
+        this.vkeys = [];
+        this._vkeys();
+    }
+    _get(key) {
+        return q(`#key_${key}`);
+    }
+    _vkeys() {
+        for (let c of this.chars) {
+            this.vkeys.push(this._get(c));
+        }
+    }
+    activate(key) {
+        this._get(key).disabled = false
+    }
+    deactivate(key) {
+        this._get(key).disabled = true;
+    }
+
+    activateAll() {
+        for (const vk of this.vkeys) {
+            vk.disabled = false;
+        }
+    }
+    deactivateAll() {
+        for (const vk of this.vkeys) {
+            vk.disabled = true;
+        }
+    }
+
+}
+
+class HangmanHelper {
+    constructor(hangman) {
+        this.figure = new FigureHandler();
+        this.virtualKeys = new VirtualKeyboard();
+        this.inputs = new InputHandler();
+        this.how = hangman;
+    }
+    hint(text) {
+        if (typeof text !== 'string' || text === "") {
+            throw "text is null";
+        }
+        q("#hint>span").innerHTML = text;
+    }
+    reset() {
+        this.figure.deAnimateAll();
+        this.virtualKeys.activateAll();
+        this.inputs.clear();
+    }
+    write() {
+        let i = 0;
+        let letters = this.how.word.split("");
+        for (let l of letters) {
+            this.inputs.setLetter(i, l);
+            i++;
+        }
+    }
+    getKey(e) {
+        // return only a valid key
+        // debugger;
+        if (/[a-zA-Z]/.test(e.key) && e.key.length === 1) {
+            return e.key;
+        }
+        if (e.code === "space") {
+            return e.code;
+        }
+        return null;
+    }
+    onKeyEvent(event) {
+        let key = this.getKey(event);
+        if (key === null) {
+            return false;
+        }
+        key = key.toLowerCase();
+        this.virtualKeys.deactivate(key);
+        this.key(key);
+    }    
+    onVirtualKeyEvent(event) {
+        // debugger;
+        event.target.disabled = true;
+        let key = event.target.innerHTML;
+        this.key(key);
+    }
+    setupKeyboard() {
+        document.addEventListener('keydown', this.onKeyEvent.bind(this));
+    }
+    setupVirtualKeyboard() {
+        // debugger;
+        for (const vk of this.virtualKeys.vkeys) {
+            vk.addEventListener('click', this.onVirtualKeyEvent.bind(this));
+        }
+    }
+    key(key) {       
+        key = key.toLowerCase();
+        if(this.how.crossed.includes(key)){
+            return false;
+        }
+        this.how.crossed.push(key);
+        if (key == "space") key = " ";
+
+        if (this.how.letters.includes(key)) {
+            for (let i = 0, len = this.how.letters.length; i < len; i++) {
+                if (this.how.letters[i] === key) {
+                    this.inputs.setLetter(i, key);
+                    this.how.letters[i] = null;
+                }
+            }
+        } else {
+            this.figure.animate(this.how.count++);//hang
+        }
+
+        let letters = this.how.letters.filter(elem => elem !== null);
+
+        let len = letters.length;
+        if (len === 0) {
+            let int = setTimeout(() => {
+                this.how.loadLevel();
+                clearTimeout(int)
+            }, 2000);
+        }
+        if (this.how.count >= 8 && len > 0) {
+            this.figure.animate(this.how.count);
+            let int = setTimeout(() => {
+                this.how.loadLevel();
+                clearTimeout(int)
+            }, 3000);
+            this.write();
+        }
+    }
+}
+
+let helper;
+
+class HangMan {
+    constructor() {
+        this.count = 0;
+        this.letters = null;
+        this.crossed = null;
+        this.word = ";"
+        this.scale = 1;
+    }
+    setup() {
+        helper = new HangmanHelper(this);
+        helper.setupKeyboard();
+        helper.setupVirtualKeyboard();
+    }
+    loadLevel() {
+        this.count = 0;
+        let rand = Math.floor(Math.random() * words.length);
+        let word = words[rand]
+        this.word = word.name;
+
+        this.letters = word.name.split("");
+        this.crossed = [];
+        let len = this.letters.length;
+
+        helper.reset();
+        helper.hint(word.type);
+
+        helper.inputs.showOnlyXInputs(len-1);        
+        helper.inputs.setLetter(0, this.letters[0]);
+
+        this.letters[0] = null;
+    }
+}
+let hm = new HangMan();
+hm.setup();
+hm.loadLevel();
